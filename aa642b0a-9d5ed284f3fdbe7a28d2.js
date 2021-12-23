ac2f76a42aa2936c9611d1367930e42abb24fe3a
timosean <timosean@naver.com>
2021-12-23 17:29:36 +0900

    Updates

---
+++ aa642b0a-9d5ed284f3fdbe7a28d2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[726],{206:function(t,e,a){a.d(e,{tzA:function(){return u}});var r=a(8922);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M23,20 L23,6 L12,15 L1,6 L1,20 L23,20 Z M12,12 L22,4 L2,4 L12,12 Z"}}]})(t)}}}]);
+//# sourceMappingURL=aa642b0a-9d5ed284f3fdbe7a28d2.js.map
